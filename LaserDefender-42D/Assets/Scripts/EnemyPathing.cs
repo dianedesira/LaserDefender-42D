79962f53b76342000569526cad7b7f6e2e8decda
Diane Desira <diane.borg@mcast.edu.mt>
2020-11-17 14:21:27 +0100

    Updated EnemyPathing script and created WaveConfig
    We set the EnemyPathing script so that each enemy can look as if it's moving along a path by moving from one coordinate to another.
    
    WaveConfig was created to store all data needed by the enemy clones only once rather than saving a copy for each enemy clone.

--- LaserDefender-42D/Assets/Scripts/EnemyPathing.cs
+++ LaserDefender-42D/Assets/Scripts/EnemyPathing.cs
@@ -5,16 +5,56 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour
 {
     [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+
+    int wayPointIndex = 0;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = waypoints[wayPointIndex].position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EnemyMove();
+    }
+
+    void EnemyMove()
+    {
+        // a check is made to make sure that there are coordinates left to traverse
+        // in the list.
+        if (wayPointIndex <= waypoints.Count - 1)
+        // if(wayPointIndex < waypoints.Count)
+        {
+            var targetPosition = waypoints[wayPointIndex].transform.position;
+
+            var movementThisFrame = moveSpeed * Time.deltaTime; // making the enemy
+            // movement frame independent (moving at the same speed on every PC).
+
+            /*
+             * MoveTowards is a method found in the Vector2 class. This method returns
+             * a temporarily coordinate so that it starts moving the object from the
+             * current position to the targetposition. Different coordinates are 
+             * constantly returned to make the object move "slowly" in a path to the 
+             * targetPosition, otherwise, the object would go immediately to the 
+             * targetPosition.
+             * The value of the coordinate depends on the speed which is passed as the
+             * last parameter.
+             */
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition,
+                movementThisFrame);
+
+            //check if current position is equal to that of the destination so that we
+            //can start moving towards the next coordinate (if there are any left)
+            if (transform.position == targetPosition)
+                wayPointIndex++;
+        }
+        else // if there are no coordinates left in our list, destroy the enemy
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ LaserDefender-42D/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Normal scripts are of type MonoBehaviour since they are used to control objects
+ * in our scene/hierarchy.
+ * 
+ * ScriptableObject scripts are used to solve the issue of Prefabs since every time
+ * a prefab is cloned, a copy of all the data for the prefab is generated and using
+ * up the memory. There are certain values which need to be repeated and copied 
+ * for every clone but there is certain values which remain the same for all 
+ * prefabs. A ScriptableObject ensures that the specified values are created only
+ * once (regardless of how many prefab clones there are) and the values can be 
+ * shared by all of the clones.
+ */
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+    //enemy
+    [SerializeField] GameObject enemyPrefab;
+
+    //the path on which to go
+    [SerializeField] GameObject pathPrefab;
+
+    //time between each spawn
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+
+    //include this random time difference between spawns
+    [SerializeField] float spawnRandomFactor = 0.3f;
+
+    //number of enemies in wave
+    [SerializeField] int numberOfEnemies = 5;
+
+    //enemy movement speed
+    [SerializeField] float enemyMoveSpeed = 2f;
+
+    /* We are going to leave the variables as private so that other scripts cannot
+     * access them and change their values. Instead, we created a method for each
+     * variable so that its value is returned and thus can be read by other scripts
+     * (the variable are READ ONLY). In fact these methods are called getter methods 
+     * since they get and return a value.
+     */
+    public GameObject GetEnemyPrefab()
+    {
+        return enemyPrefab;
+    }
+    public GameObject GetPathPrefab()
+    {
+        return pathPrefab;
+    }
+    public float GetTimeBetweenSpawns()
+    {
+        return timeBetweenSpawns;
+    }
+    public float GetSpawnRandomFactor()
+    {
+        return spawnRandomFactor;
+    }
+    public int GetNumberOfEnemies()
+    {
+        return numberOfEnemies;
+    }
+    public float GetEnemyMoveSpeed()
+    {
+        return enemyMoveSpeed;
+    }
+}
